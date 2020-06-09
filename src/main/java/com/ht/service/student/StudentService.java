53af62d8889a2f2826b9139afe62addd3801c661
wjb-github <63576455+wjb-github@users.noreply.github.com>
2020-06-09 10:31:47 +0800

    wjb

--- src/main/java/com/ht/service/student/StudentService.java
+++ src/main/java/com/ht/service/student/StudentService.java
@@ -1,7 +1,11 @@
 package com.ht.service.student;
 
 import com.ht.bean.student.Student;
-    /**
+import com.ht.util.Pager;
+
+import java.util.List;
+
+/**
  * @author 王金宝
  * @date 2020/6/8 20:42
  *  
@@ -9,16 +13,20 @@ import com.ht.bean.student.Student;
 public interface StudentService{
 
 
-    int deleteByPrimaryKey(Integer studid);
+    int deleteByPrimaryKey(Integer studid); //根据id删除学生
 
-    int insert(Student record);
+    int insert(Student record); //添加学生
 
-    int insertSelective(Student record);
+    int insertSelective(Student record); //添加部分学生信息
 
-    Student selectByPrimaryKey(Integer studid);
+    Student selectByPrimaryKey(Integer studid); //根据id查找学生信息
 
-    int updateByPrimaryKeySelective(Student record);
+    int updateByPrimaryKeySelective(Student record); //更新部分学生信息
 
-    int updateByPrimaryKey(Student record);
+    int updateByPrimaryKey(Student record); //更新学生信息
+
+    List<Student> allPageStu(Pager pager); //分页查询学生
+
+    int getTotalRow();  //获取总行数
 
 }
--- src/main/java/com/ht/web/student/StuController.java
+++ src/main/java/com/ht/web/student/StuController.java
@@ -1,10 +1,13 @@
 package com.ht.web.student;
 
+import com.ht.bean.student.Student;
 import com.ht.service.student.StudentService;
+import com.ht.util.Pager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import java.util.Map;
 
 /**
  * @author lrq
@@ -12,14 +15,33 @@ import javax.annotation.Resource;
  * 学生管理
  */
 @Controller
-@RequestMapping("Student")
+@RequestMapping("stu")
 public class StuController {
     @Resource
     private StudentService studentService;
 
     @RequestMapping("list")
-    public String list(){
-        System.out.println("？？？？？？？");
+    public String list(Pager pager, Map map){
+        pager.page(studentService.getTotalRow());
+        map.put("",studentService.allPageStu(pager));
+        return "";
+    }
+
+    @RequestMapping("add")
+    public String add(Student student){
+        studentService.insert(student);
+        return "";
+    }
+
+    @RequestMapping("upd")
+    public String upd(Student student){
+        studentService.updateByPrimaryKey(student);
+        return "";
+    }
+
+    @RequestMapping("del")
+    public String del(int stuid){
+        studentService.deleteByPrimaryKey(stuid);
         return "";
     }
 }
