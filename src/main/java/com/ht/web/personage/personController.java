38b7c72f319ec7883fb9918a919a105321fa0598
summer-love <2689105118@qq.com>
2020-06-18 15:46:20 +0800

    半口香甜

--- src/main/java/com/ht/web/personage/personController.java
+++ src/main/java/com/ht/web/personage/personController.java
@@ -1,17 +1,14 @@
 package com.ht.web.personage;
 
 import com.ht.bean.emp.Empinfo;
-import com.ht.bean.json.JsonData;
-import com.ht.service.emp.DepService;
 import com.ht.service.emp.EmpinfoService;
-import com.ht.service.emp.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 /**
  * 半口香甜吴一玄
@@ -30,8 +27,17 @@ public class personController {
      * @return
      */
     @RequestMapping("topersonage")
-    public String topersonage(Model model){
+    public String topersonage(Model model, HttpSession session){
+//        Empinfo user=(Empinfo) session.getAttribute("user");
+//        Empinfo empinfo= empinfoService.selectByPrimaryKey(user.getEmp_id());
         Empinfo empinfo= empinfoService.selectByPrimaryKey(5);
+        String[] strings=empinfo.getAddress().split("/");//前台参数，截取省 市 区
+//        for(int i=0,len=strings.length;i<len;i++){
+//            System.out.println(strings[i].toString());
+//        }
+        model.addAttribute("province",strings[0]);//省
+        model.addAttribute("city",strings[1]);//市
+        model.addAttribute("county",strings[2]);//区
         model.addAttribute("person",empinfo);
         return "personage/personage";
     }
@@ -43,13 +49,17 @@ public class personController {
      */
     @RequestMapping("update")
     @ResponseBody
-    public String update(Empinfo empinfo){
-        System.out.println("empinfo.toString() = " + empinfo.toString());
-        empinfoService.updateByPrimaryKeySelective(empinfo);
-        return "personage/personage";
+    public Integer update(Empinfo empinfo,String province,String city,String county){
+        String area=null;
+        if(province!=null){
+             area=province+"/"+city+"/"+county;//前端参数拼接省 市 区
+        }
+        empinfo.setStatus(1);
+        empinfo.setAddress(area);
+        System.out.println("empinfo" + empinfo.toString());
+        Integer update= empinfoService.updateByPrimaryKeySelective(empinfo);
+        return update;
     }
-
-
 
 
 
