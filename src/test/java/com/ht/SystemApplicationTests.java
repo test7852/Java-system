033d0b086a39b77afcc08544a21232636a9ae6eb
1323498966 <1323498966@qq.com>
2020-06-19 19:19:44 +0800

    王钟华

--- src/test/java/com/ht/SystemApplicationTests.java
+++ src/test/java/com/ht/SystemApplicationTests.java
@@ -1,13 +1,10 @@
 package com.ht;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SystemApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
